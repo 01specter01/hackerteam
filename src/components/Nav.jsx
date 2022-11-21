66471db685ef66157cb669ab9d5ff4d86596a4a5
Elena Rotari <elena.rotari@icloud.com>
2022-11-21 22:57:43 +0100

    add  nav style, add search logic and waiting for your j-son, guys.

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,14 +1,26 @@
 import { NavLink } from "react-router-dom";
 import data from "../data.json";
 import "./Nav.scss";
-export default function Nav() {
+import reactLogo from "../assets/react.svg";
+import { BsSearch } from "react-icons/bs";
+export default function Nav({ setSearch }) {
   return (
     <div className="navContainer">
+      <img src={reactLogo} alt="reactLogo" />
       {data.navItems.map((item) => (
         <NavLink key={item} to={`/root/${item}`}>
           {item[0].toUpperCase() + item.slice(1)}
         </NavLink>
       ))}
+      <div className="searchContainer">
+        <input
+          className="searchInput"
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearch((prev) => (prev = e.target.value))}
+        />
+        <BsSearch className="searchIcon" />
+      </div>
     </div>
   );
 }
--- src/components/middleComponents/Root.jsx
+++ src/components/middleComponents/Root.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { useState } from "react";
 import { Outlet } from "react-router-dom";
 import Nav from "../Nav";
+import "./Root.scss";
 
 export default function Root() {
+  const [search, setSearch] = useState("");
   return (
     <div>
-      <Nav />
-      <Outlet />
+      <Nav setSearch={setSearch} />
+      <Outlet context={search} />
     </div>
   );
 }
