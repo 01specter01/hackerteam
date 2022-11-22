fe55cd59ff1d413006f43be18508f98bf0b5af8a
Elena Rotari <elena.rotari@icloud.com>
2022-11-22 19:22:27 +0100

    changed search Logik , extract to a new Component

--- src/components/middleComponents/Home.jsx
+++ src/components/middleComponents/Home.jsx
@@ -1,16 +1,17 @@
 import { useOutletContext } from "react-router-dom";
+import Tools from "../Tools";
 import Publicty from "./Publicty";
 
 export default function Home() {
   const search = useOutletContext();
-  const text =
+  let text =
     "On this page we've listed some React-related communities that you can be a part of; see the other pages in this section for additional online and in-person learning materials";
 
   return (
     <div className="home">
-      {text.slice(0, text.indexOf(search))}
-      <span className="searched">{search}</span>
-      {text.slice(text.indexOf(search) + search.length)}
+      <p>
+        <Tools str={text} search={search} />
+      </p>
       <Publicty />
     </div>
   );
