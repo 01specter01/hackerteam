24d24614181696e22c549a2ac9db61ab92e65bbe
Elena Rotari <elena.rotari@icloud.com>
2022-11-22 09:05:14 +0100

    context folder und function add

--- src/components/middleComponents/CodingSubscription.jsx
+++ src/components/middleComponents/CodingSubscription.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function CodingSubscription() {
-  return <div>CodingSubscription</div>;
+  return (
+    <div>
+      Here we will create 3 cards , where our customers could subscribe us.{" "}
+    </div>
+  );
 }
