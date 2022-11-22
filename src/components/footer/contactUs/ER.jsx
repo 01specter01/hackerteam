6c1fedd45bcb8155c8446dc79a01c5e0e6aec469
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-22 21:22:37 +0100

    footerpages

---
+++ src/components/footer/contactUs/ER.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function ER() {
+    return <div>ER</div>;
+}
---
+++ src/components/footer/contactUs/YA.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function YA() {
+    return <div>YA</div>;
+}
