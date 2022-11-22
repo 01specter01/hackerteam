72d54a671a74286b9f03aa86c966f6dd879fdcf2
Elena Rotari <elena.rotari@icloud.com>
2022-11-22 14:36:30 +0100

    :

--- src/components/middleComponents/Publicty.jsx
+++ src/components/middleComponents/Publicty.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function Publicty() {
-  return <div className="publicity">Publicty</div>;
+  return (
+    <div className="publicity">
+      <h1>Subscribe Us</h1>
+    </div>
+  );
 }
