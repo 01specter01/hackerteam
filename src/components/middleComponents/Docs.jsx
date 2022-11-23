c5663181439a5aa516b24ea00a55cc26aecbc5b4
yaaroub <yarobdaoud1997@gmail.com>
2022-11-23 09:46:22 +0100

    korregieren

--- src/components/middleComponents/Docs.jsx
+++ src/components/middleComponents/Docs.jsx
@@ -1,5 +1,4 @@
 import data from "../../data.json";
-<<<<<<< HEAD
 import { useOutletContext } from "react-router-dom";
 import "./Docs.scss";
 import Publicty from "./Publicty";
@@ -8,12 +7,6 @@ export default function Docs() {
   const search = useOutletContext();
   return (
     <div className="docs">
-=======
-
-export default function Docs() {
-  const search = useOutletContext();
-  return ( <div className="docs">
->>>>>>> 6dac6f38098f2d9c19d2125b5f5474baa4a9344f
       <div className="rightContainer">
         {data.docs.map((el) => (
           <div className="docContainer" key={el.title}>
@@ -30,9 +23,5 @@ export default function Docs() {
       </div>
       <Publicty />
     </div>
-<<<<<<< HEAD
-=======
-
->>>>>>> 6dac6f38098f2d9c19d2125b5f5474baa4a9344f
   );
 }
