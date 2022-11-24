ce3f559c007522615cfd4c8126f42b4e5dae8fbe
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-24 15:41:48 +0100

    hompage mockup

--- src/components/middleComponents/Home.jsx
+++ src/components/middleComponents/Home.jsx
@@ -3,16 +3,17 @@ import Tools from "../Tools";
 import Publicty from "./Publicty";
 
 export default function Home() {
-  const [search] = useOutletContext();
-  let text =
-    "On this page we've listed some React-related communities that you can be a part of; see the other pages in this section for additional online and in-person learning materials";
+    const [search] = useOutletContext();
+    let text =
+        "On this page we've listed some React-related communities that you can be a part of; see the other pages in this section for additional online and in-person learning materials";
 
-  return (
-    <div className="home">
-      <p>
-        <Tools str={text} search={search} />
-      </p>
-      <Publicty />
-    </div>
-  );
+    return (
+        <div className="home">
+            <p>
+                <h1>Welcome to Hackerteam, the platform to learn react</h1>
+                <Tools str={text} search={search} />
+            </p>
+            <Publicty />
+        </div>
+    );
 }
