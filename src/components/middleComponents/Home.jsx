15426cbc492566500fff4785b41705a9c1fcece6
Leo Volwahsen <volwahsenl@gmail.com>
2022-11-24 17:11:14 +0100

    home page text

--- src/components/middleComponents/Home.jsx
+++ src/components/middleComponents/Home.jsx
@@ -6,15 +6,28 @@ import "./Home.css";
 export default function Home() {
     const [search] = useOutletContext();
     let text =
-        "React is a Javascript library for building user interfaces, the Hackerteam is commited to helping beginners to get to know how to best use React and itś components.";
-    // "On this page we've listed some React-related communities that you can be a part of; see the other pages in this section for additional online and in-person learning materials";
+        "React is a Javascript library for building user interfaces, the Hackerteam is commited to helping beginners to get to know how to best use React and it's components.On this page we've listed some React-related communities that you can be a part of; see the other pages in this section for additional online and in-person learning materials. ";
 
     return (
         <div className="home">
-            <p>
-                <h1>Welcome to Hackerteam, the platform to learn react</h1>
+            <span>
+                <h1>Welcome to Hackerteam, the platform to learn React</h1>
                 <Tools str={text} search={search} />
-            </p>
+                <br />
+                <br />
+                <h3>How does one go about creating a react app?</h3>
+                <ol>
+                    <li>
+                        Download NodeJS from official website:
+                        https://nodejs.org/en/
+                    </li>
+                    <li>Install text editor i.e. Visual-Studio-Code</li>
+                    <li>Wright in Terminal: create-react-app my-react-app</li>
+                </ol>
+                <br />
+                <br />
+                <h3>How does one go about creating a react app?</h3>
+            </span>
             <Publicty />
         </div>
     );
