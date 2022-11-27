83629a5bd9fac803b11dce4a8bd328a87754009a
Elena Rotari <elena.rotari@icloud.com>
2022-11-27 23:59:09 +0100

    done 100% fro my side

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,21 +1,42 @@
-import { json, NavLink, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { NavLink, useNavigate } from "react-router-dom";
 import data from "../data.json";
 import "./Nav.scss";
 import reactLogo from "../assets/react.svg";
 import { BsSearch } from "react-icons/bs";
 import { MdLogin, MdLogout } from "react-icons/md";
+import { IoDocumentTextOutline, IoVideocamOutline } from "react-icons/io5";
+import { TbBellRinging } from "react-icons/tb";
+import { MdOutlineContactMail } from "react-icons/md";
 
 export default function Nav({ setSearch, logged, setLogged }) {
   const navigator = useNavigate();
+  const getIcon = (index) => {
+    switch (index) {
+      case 1:
+        return <IoDocumentTextOutline />;
+      case 2:
+        return <IoVideocamOutline />;
+      case 3:
+        return <TbBellRinging />;
+      case 4:
+        return <MdOutlineContactMail />;
+    }
+  };
   return (
     <div className="navContainer">
-      <img src={reactLogo} alt="reactLogo" />
-      {data.navItems.map((item) => (
-        <NavLink key={item} to={`/my/${item}`}>
-          {item[0].toUpperCase() + item.slice(1)}
+      <div>
+        <NavLink to="/my/react">
+          <img src={reactLogo} alt="reactLogo" />
         </NavLink>
-      ))}
+        {data.navItems.map((item, i) => (
+          <NavLink key={item} to={`/my/${item}`}>
+            <span className="textNav">
+              {item[0].toUpperCase() + item.slice(1)}
+            </span>
+            <span className="iconNav">{getIcon(i)}</span>
+          </NavLink>
+        ))}
+      </div>
       <div className="searchContainer">
         {/* Toggle icon Login vs Logout + userName in dependence of state logged => userObject || false*/}
         {!logged ? (
@@ -25,7 +46,10 @@ export default function Nav({ setSearch, logged, setLogged }) {
           />
         ) : (
           <>
-            <span style={{ color: "white", margin: "10px" }}>
+            <span
+              style={{ color: "white", margin: "10px" }}
+              className="username"
+            >
               {JSON.parse(localStorage.getItem("user")).firstName}
             </span>
             <MdLogout
