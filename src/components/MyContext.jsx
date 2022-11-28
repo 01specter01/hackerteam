5296e3925bda7d102bbca75c9dfd936d95153f31
Elena Rotari <elena.rotari@icloud.com>
2022-11-28 09:26:21 +0100

    last one

--- src/components/MyContext.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useState } from "react";
-const Context = createContext();
-export default function MyContext({ children }) {
-  const user = useState(" ");
-
-  return <Context.Provider value={user}>{children}</Context.Provider>;
-}
--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import MyContext from "./components/MyContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      <MyContext>
-        <App />
-      </MyContext>
+      <App />
     </BrowserRouter>
   </React.StrictMode>
 );
